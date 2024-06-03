b824a784c8c4122fec25a8db18bc3a25d8e19ff9
Uwais Jawed <uwaisjawed8081@gmail.com>
2024-06-03 22:42:38 +0530

    add vercel json

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // server.js
 const express = require("express");
 const path = require("path");
-const getTrendingTopics = require("../scripts/seleniumScript");
+const getTrendingTopics = require("./scripts/seleniumScript");
 require("dotenv").config();
 
 const app = express();
@@ -9,28 +9,9 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("."));
 
-// app.get("/run-selenium", (req, res) => {
-//   // Calling the getTrendingTopics function
-//   getTrendingTopics()
-//     .then((data) => {
-//       // Sending the result back to the client
-//       res.send(data);
-//     })
-//     .catch((error) => {
-//       console.error("Error running Selenium script:", error);
-//       res.status(500).send("Error running Selenium script");
-//     });
-// });
-
-// app.get("/", (req, res) => {
-//   console.log("Server is running on http://localhost:3000");
-
-//   res.sendFile(path.join(__dirname, "index.html"));
-// });
-
 app.all("/", async (req, res) => {
   if (req.method === "GET") {
-    res.sendFile(path.join(__dirname, "../public/index.html"));
+    res.sendFile(path.join(__dirname, "index.html"));
   } else if (req.method === "POST") {
     try {
       console.log("Received request to run Selenium script");
