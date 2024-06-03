498bd163aff20008b9619f46c8f32fe1946fd67a
Uwais Jawed <uwaisjawed8081@gmail.com>
2024-06-03 21:30:53 +0530

    Changed get to post

--- server.js
+++ server.js
@@ -9,21 +9,41 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.static("."));
 
-app.get("/run-selenium", (req, res) => {
-  // Calling the getTrendingTopics function
-  getTrendingTopics()
-    .then((data) => {
-      // Sending the result back to the client
-      res.send(data);
-    })
-    .catch((error) => {
+// app.get("/run-selenium", (req, res) => {
+//   // Calling the getTrendingTopics function
+//   getTrendingTopics()
+//     .then((data) => {
+//       // Sending the result back to the client
+//       res.send(data);
+//     })
+//     .catch((error) => {
+//       console.error("Error running Selenium script:", error);
+//       res.status(500).send("Error running Selenium script");
+//     });
+// });
+
+// app.get("/", (req, res) => {
+//   console.log("Server is running on http://localhost:3000");
+
+//   res.sendFile(path.join(__dirname, "index.html"));
+// });
+
+app.all("/", async (req, res) => {
+  if (req.method === "GET") {
+    res.sendFile(path.join(__dirname, "index.html"));
+  } else if (req.method === "POST") {
+    try {
+      console.log("Received request to run Selenium script");
+      const data = await getTrendingTopics();
+      console.log("Selenium script executed successfully:", data);
+      res.json(data);
+    } catch (error) {
       console.error("Error running Selenium script:", error);
       res.status(500).send("Error running Selenium script");
-    });
-});
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
+    }
+  } else {
+    res.status(405).send("Method Not Allowed");
+  }
 });
 
 app.listen(PORT, () => {
